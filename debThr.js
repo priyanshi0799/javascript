2c562be1ca85b1c42f01ed278d8cb13fbf792718
priyanshi0799 <priyanshigupta997@gmail.com>
2022-09-19 01:37:25 +0530

    Throttling

--- debThr.js
+++ debThr.js
@@ -15,3 +15,24 @@ const debounce = function(fn, timer){
 const magicFunc = debounce(handleChange, 1000);
 
 
+const handleClick = (val) => {
+    console.log("Hii", val);
+}
+
+const throttling = function(fn, timer){
+    let flag = true;
+    return function(...args){
+        if(flag){
+            fn.apply(this, args);
+            flag = false;
+
+            setTimeout(() => {
+                flag = true
+            }, timer);
+        }
+
+        
+    };
+};
+
+const magicClick = throttling(handleClick, 3000);
