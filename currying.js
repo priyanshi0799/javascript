d47ef5bd943aae82993e821d883f03d685fd5327
priyanshi0799 <priyanshigupta997@gmail.com>
2022-09-23 17:54:44 +0530

    Questions on currying

--- currying.js
+++ currying.js
@@ -39,3 +39,33 @@ function multiply(a,b){
 //here, multiply is used to create other functions
 let multiplyByTwo = multiply.bind(this, 8);
 console.log(multiplyByTwo(2));
+
+
+//Question
+// mul(2)(3)(4)
+function mul(a){
+    if(arguments.length > 1)
+        return arguments[2]*arguments[0]*arguments[1];
+    if(Array.isArray(arguments[0])){
+        return arguments[0][0]*arguments[0][1]*arguments[0][2]
+    } 
+    return function(b){
+        return function(c){
+            return a*b*c;
+        }
+    }
+}
+
+console.log('Call', mul(1)(2)(4))
+console.log('Array', mul([2,3,4]))
+console.log('Comma separated', mul(2,3,4))
+
+//Question
+function multi(a){
+    return function(b){
+        if(b)
+            return multi(a*b)
+        return a;
+    }
+}
+console.log(multi(2)(3)(4)(5)(10)());
